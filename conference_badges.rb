e6b058cf9ce5e838dcb4c428e34a77b52669224e
Jesse Brinkley <jbrink28@gmail.com>
2020-08-26 01:56:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,3 +1,11 @@
 def badge_maker(name)
   "Hello, my name is #{name}."
 end
+
+def batch_badge_creator(attendees)
+  greet_attendees = []
+  attendees.each do |name|
+    greet_attendees.push("Hello, my name is #{name}.")
+  end
+  greet_attendees
+end
